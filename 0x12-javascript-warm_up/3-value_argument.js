2b0aaadee4f256e9adaf6c1b28fd2f761fcdf44c
Sunnywayne7 <akandesunday70@gmail.com>
2023-06-13 18:59:15 +0100

    javascript warmup

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+let len = process.argv.length;
+
+if (len < 3) {
+	console.log("No argument")
+}
+
+else {
+	console.log(process.argv[2])
+}
+
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const process = require('process');
+let processe = process.argv
+console.log(processe[2] + " " + "is" + " " + processe[3])
+
